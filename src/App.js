0c8f2ac3884c3723dd0aa8f39bfc9d241057597a
FestimZylfiu <fz57195@ubt-uni.net>
2025-04-16 20:21:48 +0200

    Added a new Terms Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
+import PaymentPage from './pages/PaymentPage';
+import TermsPage from './pages/TermsPage';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/kako-da-platam' element={<PaymentPage />} />
+        <Route path='/opsti-uslovi' element={<TermsPage />} />
       </Routes>
       <Footer />
     </Router>
