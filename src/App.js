b6086757b63d8dc0467d2e40b4bd5581a009ce5a
FestimZylfiu <fz57195@ubt-uni.net>
2025-04-24 22:48:26 +0200

    Added new component Testimonial slider

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import PaymentPage from './pages/PaymentPage';
 import TermsPage from './pages/TermsPage';
 import TariffPage from './pages/TariffPage';
 import ContactPage from './pages/ContactPage';
-// import PrivacyPage from './pages/PrivacyPage';
-// import ApplyPage from './pages/ApplyPage';
-// import CookiePage from './pages/CookiePage';
+import PrivacyPage from './pages/PrivacyPage';
+import ApplyPage from './pages/ApplyPage';
+import CookiePage from './pages/CookiePage';
 
 function App() {
   return (
@@ -21,9 +21,9 @@ function App() {
         <Route path='/opsti-uslovi' element={<TermsPage />} />
         <Route path='/tarifnik' element={<TariffPage />} />
         <Route path='/kontakt' element={<ContactPage />} />
-        {/* <Route path='/privacy-policy' element={<PrivacyPage />} />
+        <Route path='/privacy-policy' element={<PrivacyPage />} />
         <Route path='/cookie-policy' element={<CookiePage />} />
-        <Route path='/kako-da-apliciram' element={<ApplyPage />} /> */}
+        <Route path='/kako-da-apliciram' element={<ApplyPage />} />
       </Routes>
       <Footer />
     </Router>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import "../components/Main.css"
 import "./HomePage.css"
 import kredo2 from "../assets/images/kredo-2.png"
 import { useLocation } from 'react-router-dom';
+import Testimonials from '../components/Testimonials';
 
 function HomePage() {
   const [openDropdown, setOpenDropdown] = useState(null);
@@ -134,6 +135,7 @@ function HomePage() {
     </div>
   </div>
 </div>
+<Testimonials/>
     <div className='gray-container'>
   <div className='container'>
     <div className='gray-text'>
